24043519a209b7bd01c088263722be663cd19cc7
Keilerg <keilere-garciaa@unilibre.edu.co>
2024-04-24 08:07:03 -0500

    new

--- controllers/EmpresaController.py
+++ controllers/EmpresaController.py
@@ -1,10 +1,52 @@
-from flask import Blueprint, render_template, request, redirect
-from config.db import app, db, ma
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from config.db import db, app
 from models.EmpresaModel import EMP, EMPSchema
 
-ruta_empresa = Blueprint('route_empresa', __name__)
+ruta_empresa = Blueprint('ruta_empresa', __name__)
 
-EMP_schema = EMPSchema()
-EMP_schema = EMPSchema(many=True)
+EMP_schema = EMPSchema() 
+EMPS_schema = EMPSchema(many=True) 
 
+@app.route('/empresas/crear', methods=['POST'])
+def crear_empresa():
+    if request.method == 'POST':
+        
+        nombre = request.form['name_emp']
+        mail = request.form['mail_emp']
+        ubicacion = request.form['ubicacion']
+        status = request.form['status']
+        IVA = int(request.form['IVA'])  
+        fecha_inicio = request.form['fecha_Inicio']
+        fecha_final = request.form['fecha_final']
+
+        nueva_empresa = EMP(
+            name_emp=nombre,
+            mail_emp=mail,
+            ubicacion=ubicacion,
+            status=status,
+            IVA=IVA,
+            fecha_Inicio=fecha_inicio,
+            fecha_final=fecha_final
+        )
+
+        db.session.add(nueva_empresa)
+        db.session.commit()
+
+    return redirect('/empresas')
+
+
+@app.route('/empresas', methods=['GET'])
+def ver_empresas():
+    empresas = EMP.query.all()
+    return render_template('./Admin/portalAdministrativo.html', empresas=empresas)
+
+@app.route('/empresas/buscar', methods=['GET'])
+def buscar_empresas():
+    termino = request.args.get('termino')
+
+    empresas_filtradas = EMP.query.filter(
+        (EMP.name_emp.like(f"%{termino}%")) | (EMP.companyid.like(f"%{termino}%"))
+    ).all()
+
+    return render_template("empresa_tbody.html", empresas=empresas_filtradas)
 
--- models/UserModel.py
+++ models/UserModel.py
@@ -6,14 +6,19 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(50))
+    rol = db.Column(db.String(30))
     
-    def __init__(self, user, password):
+    def __init__(self, user, password, rol):
       self.user = user
       self.password = password
+      self.rol = rol
+    
+    def __repr__(self):
+        return f'<User {self.user}>'
     
 with app.app_context():
     db.create_all()
 
 class UsersSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'user', 'password')
+        fields = ('id', 'user', 'password', 'rol')
