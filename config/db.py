a5dbf23c715e4f90dbf23a5db4916e8fc1fb1b19
Keilerg <keilere-garciaa@unilibre.edu.co>
2024-04-25 22:24:05 -0500

    actualizacion

--- config/db.py
+++ config/db.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123Sebas@127.0.0.1/innovashop'   #(Base de datos Sebastian)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1/web'    #(Base de datos Keiler)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234567890@localhost/web'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234567890@localhost/web'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = "claveSecreta"
