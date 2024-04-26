2ee2f828f80e76654d46c6f10bdf537786cf086f
Keilerg <keilere-garciaa@unilibre.edu.co>
2024-04-25 22:13:35 -0500

    subi

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, redirect, request, jsonify, json, session, render_templ
 from config.db import app, db
 
 from controllers.UserController import ruta_user, User
-from controllers.ArticulosController import ruta_articulos
+from controllers.ProductosController import ruta_articulos
 from controllers.AdministradorController import ruta_administrador
 from controllers.EmpresaController import ruta_empresa
 from controllers.ProveedoresController import ruta_proveedores
 from controllers.VendedorController import ruta_vendedor
+from controllers.ClienteController import ruta_clientes
 
 app.register_blueprint(ruta_user, url_prefix="/controller")
 app.register_blueprint(ruta_articulos, url_prefix="/controller")
@@ -14,6 +15,7 @@ app.register_blueprint(ruta_administrador, url_prefix="/controller")
 app.register_blueprint(ruta_empresa, url_prefix="/controller")
 app.register_blueprint(ruta_proveedores, url_prefix="/controller")
 app.register_blueprint(ruta_vendedor, url_prefix="/controller")
+app.register_blueprint(ruta_clientes, url_prefix="/controller")
 
 @app.route('/', methods=['GET'])
 def index():
--- models/ClienteModel.py
+++ models/ClienteModel.py
@@ -1,23 +1,28 @@
 from config.db import app, db, ma 
 
-class Proveedor(db.Model):
-    __tablename__ = 'Proveedor'
+class Cliente(db.Model):
+    __tablename__ = 'Cliente'
     
     id = db.Column(db.Integer, primary_key=True)
     nombre= db.Column(db.String(50), unique=True)
     email = db.Column(db.String(50))
     direccion = db.Column(db.String(50))
     telefono = db.Column(db.Integer)
+    user = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
     
-    def __init__(self, nombre, email, direccion, telefono ):
+    def __init__(self, id,nombre, email, direccion, telefono, user, password):
+      self.id = id
       self.nombre = nombre
       self.email = email
       self.direccion = direccion
       self.telefono = telefono
+      self.user = user
+      self.password = password
     
 with app.app_context():
     db.create_all()
 
-class ProveedoresSchema(ma.Schema):
+class ClientesSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'nombre', 'email', 'direccion', 'telefono')
+        fields = ('id', 'nombre', 'email', 'direccion', 'telefono', 'user','password')
